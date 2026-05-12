93c8b045ee8bda8665b8bad45e9c95c97d34e293
uncoalesced <uncoalesced@gmail.com>
2026-05-13 01:08:31 +0530

    chore: purge legacy utils directory superseded by core_system architecture

--- utils/safe_shutdown.py
+++
@@ -1,7 +0,0 @@
-# in utils/safe_shutdown.py
-def safe_shutdown():
-    """Placeholder for safe shutdown procedures."""
-    print("Safe shutdown procedure called.")
-    # In the future, you can add database connection closing,
-    # saving state, etc. here.
-    exit()
